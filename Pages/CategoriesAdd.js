4f067f3d780f126bfa03a59b53c8db3b5c52bc67
ogi-98 <oguzkaya99@hotmail.com>
2021-05-13 01:40:07 +0300

    CategoryAdd functions and UI

--- Pages/CategoriesAdd.js
+++ Pages/CategoriesAdd.js
@@ -1,24 +1,119 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, TextInput, Alert} from 'react-native';
+import { Input, Button , Icon} from 'react-native-elements';
+import apiFuncs from '../env/apiFunctions';
 
 
-export default function CategoriesAdd({ navigation }) {
+
+export default function CategoriesAdd({ navigation :{ goBack} }) {
+
+
+  // Name and Description for adding
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  // Succesful Adding
+  const [Success,SetSuccess] = useState(false)
+  // buton activity indicator 
+  const [loading, setloading] = useState(false)
+
+
+
+  const addCategoryIcon = () => {
+    if (Success) {
+      return (
+        <Icon reverse name='check-circle' type='material' color='green' ></Icon>
+      );
+    } else {
+      return (
+        <Icon name='add-circle' type='material' color='#4285F4' raised onPress={()=> addData({
+          name: name != '' ? name : '',
+          description: description != '' ? description: "",
+        }) }></Icon>
+      );
+    }
+  }
+
+
+  const addData = (data) => {
+
+    if (name != '' || description != '') {
+      setloading(true)
+      apiFuncs.post('api/categories/', data)
+      .then((result)=>{
+        setloading(false)
+        SetSuccess(true)
+        setName('')
+        setDescription('')
+      
+        Alert.alert(
+        "Success!",
+        "Successful Adding!",
+        [ { text: "OK", onPress: () => goBack() } ]
+        )
+      
+      })
+    } else {
+      Alert.alert(
+        "All Fields are empty!",
+        "You must fill least one row.",
+        [ { text: "Cancel" } ]
+      )
+    }
+
+    
+
+  }
+
+
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
 
-        {/* 1. box */}
-        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between',marginLeft:10,
-            marginRight:10, marginTop:10 }}>
-            <Text style={{ color: 'black', fontWeight:'bold', fontSize: 24 }}>CategoriesAdd</Text>
-            
-        </View>
+      <ScrollView>
 
-        {/* 2. box */}
-        <View style={{ flex: 1, marginLeft:10, marginRight:10}}>
-            <Text h2 style={{fontWeight:'bold', fontSize:17}}>CategoriesAdd</Text>
-            <Text h4 style={{color:'gray', fontSize:17}}>CategoriesAdd</Text>
+                
+          
+      {/* 1. box */}
+      <View style={{ flex: 1, alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between',marginLeft:10,
+          marginRight:10, marginTop:20 , marginBottom: 20}}>
+          <Text style={{ color: '#4285F4', fontWeight:'bold', fontSize: 24 }}>Category Add</Text>
+          {addCategoryIcon()}
+      </View>
+      {/* recycling  autorenew */}
+
+      {/* 2. box */}
+      <Text style={{marginBottom:5, marginLeft:10, textDecorationLine:'underline'}}>Category Preview:</Text>
+      <View style={{ flex: 1, alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between',marginLeft:10,
+          marginRight:10, marginTop:20 , marginBottom: 10, backgroundColor:'white', padding:8, borderRadius:8}}>
+          <View>
+            {name != '' ? <Text h2 style={{fontWeight:'300', fontSize:17, fontWeight:'bold',}}>{name}</Text> : <Text h2 style={{fontWeight:'300', fontSize:17, fontWeight:'bold',}}>Write name</Text>} 
+            {description != '' ? <Text h4 style={{fontWeight:'300', fontSize:17,}}>{description}</Text> : <Text h4 style={{fontWeight:'300', fontSize:17,}}>Write description</Text>} 
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <Icon name='delete' color='red' containerStyle={{backgroundColor:'transparent', padding:8}}/>
+            <Icon name='chevron-right' color='gray' containerStyle={{backgroundColor:'transparent', padding:8}}/>
+          </View>
+          
+      </View>
+
+
+
+      <View style={{padding:10, marginTop:20}} >
+        <Input placeholder='Name'  onChangeText={text => setName(text.trim())} ></Input>
+        <View style={{borderWidth: 1.5, borderColor:'gray', backgroundColor:'white',borderRadius:10, marginLeft:5, marginRight:5}} >
+          <TextInput placeholder='Description' maxLength={40}  multiline={true} style={{minHeight:80, padding:10}}
+            onChangeText={ text => setDescription(text.trim())}  ></TextInput>
         </View>
+      </View>
+
+      <Button title='Add Category' style={{padding:30}} loading={loading} onPress={()=> addData({
+        name: name != '' ? name : "",
+        description: description != '' ? description: "",
+      })}  ></Button>
+
+
+      </ScrollView>
 
     </SafeAreaView>
 );
