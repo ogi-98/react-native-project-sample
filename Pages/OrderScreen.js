5d633ccf6357879b85dd0552dda3a6c9506435a6
ogi-98 <oguzkaya99@hotmail.com>
2021-05-14 01:11:17 +0300

    OrderScreen UI and GET orders with limit

--- Pages/OrderScreen.js
+++ Pages/OrderScreen.js
@@ -1,9 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView,TouchableOpacity } from 'react-native';
+import React, {useState,useEffect} from 'react';
+import { StyleSheet, SafeAreaView, ScrollView, Alert, ActivityIndicator, RefreshControl, View, Text, TouchableOpacity, Button } from 'react-native';
+import { Icon, ListItem, SearchBar } from 'react-native-elements';
+import apiFuncs from '../env/apiFunctions';
 
 
 export default function OrderScreen({ navigation }) {
+
+
+   // order array useState constant
+  const [order, setOrder] = useState([]);
+  const [filteredOrder, setFilteredOrder] = useState([]);
+  //this is For ActivityIndicator
+  const [animating,setAnimating] = useState(Boolean)
+  //orders details
+  const [Detail, seTDetail] = useState([])
+
+  const [refreshing, setRefreshing] = useState(false);
+  const refreshStarting = () => {
+    setRefreshing(true)
+    // this is for get Category
+    apiFuncs.get('api/orders/?_limit=15')
+      .then((data) => {
+        setFilteredOrder(data)
+        setOrder(data)
+        setRefreshing(false)
+      })
+  }
+
+
+
+  useEffect(() => {
+
+    fillDataFromWeb();
+
+  }, []);
+  const fillDataFromWeb = () => {
+    setAnimating(true)
+    // this is for get Category
+      apiFuncs.get('api/orders/?_limit=15')  // learn limit from https://stackoverflow.com/questions/52842039/how-to-limit-the-amount-of-data-returned-from-a-json-file-using-fetch/52842411
+      .then((data) => {
+        setFilteredOrder(data);
+        setOrder(data)
+        setAnimating(false)
+      })
+    
+    
+
+  }// fillDataFromWeb
+
+  const getDetailsFromWeb = (i) => {
+
+    apiFuncs.get('api/orders/' + i)
+    .then((data) => {
+      seTDetail(data)
+    })
+
+  }
 
   
 
@@ -11,24 +64,93 @@ export default function OrderScreen({ navigation }) {
     <SafeAreaView style={{ flex: 1 }}>
 
         {/* 1. box */}
-        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between',marginLeft:10,
+        {/* <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between',marginLeft:10,
             marginRight:10, marginTop:10 }}>
-            <Text style={{ color: 'black', fontWeight:'bold', fontSize: 24 }}>OrderScreen</Text>
             
-        </View>
+        </View> */}
 
-        {/* 2. box */}
-        <View style={{ flex: 1, marginLeft:10, marginRight:10}}>
-            <Text h2 style={{fontWeight:'bold', fontSize:17}}>OrderScreen</Text>
-            <Text h4 style={{color:'gray', fontSize:17}}>OrderScreen</Text>
-        </View>
-        <View style={{flex:2, margin: 10}}>
-          <TouchableOpacity style={{ backgroundColor:'red', padding:20, borderRadius:10}} >
-            <View>
-              <Text style={styles.flexBoxTextStyle2}>OrderScreen</Text>
-            </View>
-          </TouchableOpacity>
-        </View>
+        <ScrollView>
+          <RefreshControl refreshing={refreshing} onRefresh={() => refreshStarting()}/>
+
+        {
+          filteredOrder.map((item , key ) => (
+            <TouchableOpacity
+            key= {item.id}
+            style={styles.container}
+            >
+              <View style={{margin:5,}}>
+                {/* 1. Row */}
+                <View style={{flex: 0.5, flexDirection:'row', justifyContent: 'space-between',alignSelf: 'stretch'}}>
+                  
+                <Text>Order ID: {item.id}</Text>
+                <Text >Order Date: {item.orderDate.substring(0, 11)}</Text>
+
+                </View>
+
+                <Icon name='chevron-right' style={{alignSelf: 'flex-end',marginTop:20}}  />
+
+              <View style={{flexDirection:'row', justifyContent: 'space-between',alignSelf: 'stretch',marginTop:20}} >
+              {/* 2. Row */}
+              <View style={{flex: 0.5, flexDirection:'column', justifyContent: 'space-between',alignItems:'flex-start'}}>
+                                
+                <Text >Arrived: {item.requiredDate.substring(0, 11)}</Text>
+                <Text >Shipped: {item.shippedDate.substring(0, 11)}</Text>
+
+                </View>
+
+                {/* 3. Row */}
+                <View style={{flex: 0.5, flexDirection:'column', justifyContent: 'space-between',alignItems:'flex-end'}}>
+                  
+                <Text>Ship Name: {item.shipName}</Text>
+                <Text >Customer Id: {item.customerId}</Text>
+
+                </View>
+              </View>
+                
+
+              </View>
+
+              <View style={{flexDirection:'row', justifyContent: 'space-between',alignSelf: 'stretch', backgroundColor:'red', margin:10, borderRadius:10}}>
+              <Button title='Delete Order' color='white'/>
+              <Icon name='delete' color='white' containerStyle={{backgroundColor:'transparent', padding:8}}/>
+              </View>
+
+              
+
+            </TouchableOpacity>
+          ))
+        }
+
+
+
+
+
+
+
+{/* 
+          {filteredOrder.map((item , key ) => (
+            <ListItem key = {key}>
+              <ListItem.Content backgroundColor='lightgray' padding={50}>
+
+              <View style={{flexDirection:'row', justifyContent:'space-between', flex:1.0}}>
+              <ListItem.Title style={{fontWeight:'bold'}}>Customer Id: {item.customerId}</ListItem.Title>
+              <Text>Order Date: {item.orderDate}</Text>
+              
+              <Text>Customer Id: {item.customerId}</Text>
+              <Text>Ship Name: {item.shipName}</Text>
+              </View>
+              <ListItem.Subtitle style={{ color:'gray', fontWeight:'300'}}>Ship Name: {item.shipName}</ListItem.Subtitle>
+              </ListItem.Content>
+              
+
+
+            </ListItem>
+            console.log("shipname:");
+            console.log(item.shipName);
+            console.log("item.detail.productId:");
+            console.log(item.details.productId);
+          ))} */}
+        </ScrollView>
 
     </SafeAreaView>
 );
@@ -37,14 +159,16 @@ export default function OrderScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'lightgray',
+    minHeight:120,
+    margin: 5,
+    borderRadius:8,
   },
-  flexBoxTextStyle1:{
-    color: 'black', 
+  DeleteButonStyle:{
+    backgroundColor: 'red', 
+    color:'white',
     fontWeight:'bold',
-    fontSize: 17,
+    borderRadius: 10,
   },
   flexBoxTextStyle2:{
     color: 'white', 
